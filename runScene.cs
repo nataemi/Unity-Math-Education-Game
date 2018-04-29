e9d2705800c26f127f01b8bcb35955edfd9853de
nataemi <38769153+nataemi@users.noreply.github.com>
2018-04-29 23:35:05 +0200

    Add automatic generating of pick game scene

---
+++ runScene.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class runScene : MonoBehaviour
+{
+	public static void NextScene(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+}
